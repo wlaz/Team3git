bf3b028eb27043053b1a4f39fb786a2f09761dc6
DominikVans <dominik.jakubaszek1998@gmail.com>
2022-10-30 15:01:40 +0100

    add feature age

--- apka.py
+++ apka.py
@@ -8,15 +8,17 @@ if wiek >= 18:
     print("Witamy!")
 region = input("Podaj region EUR lub USA: ")
 region= str(region)
-if region == "EUR" and wiek > 21:
-    print("pochodzisz z regionu EUR i masz wiecej niż 21 lat wiec dostajesz super ceny dla EUR")
+if region == "EUR" and wiek > 18:
+    print("pochodzisz z regionu EUR i masz wiecej niż 18 lat wiec dostajesz super ceny dla EUR")
 elif region == "USA" and wiek > 21:
-    print("pochodzisz z regionu USA i masz wiecej niż 21 lat wiec dostajesz super ceny dla EUR")
-elif region !="EUR" and wiek>21:
-    print("nie pochodzisz z regionu EUR i  ale masz więcej niż 21 wiec nie dostajesz super ceny dla EUR")
-elif region =="EUR" and wiek <=21:
-    print("pochodzisz z regionu EUR ale masz 21 lub mniej lat wiecj nie dostajesz super ceny dla USA ")
-elif region =="USA" and wiek <=21:
+    print("pochodzisz z regionu USA i masz wiecej niż 21 lat wiec dostajesz super ceny dla USA")
+elif region !="EUR" and wiek>18:
+    print("nie pochodzisz z regionu EUR i  ale masz więcej niż 18 wiec nie dostajesz super ceny dla EUR")
+elif region!="USA" and wiek>21:
+    print(" nie pochodzisz z regionu usa ale masz wiecej niz 21 , wiec nie dostajesz ceny dla usa")
+elif region =="EUR" and wiek <18:
+    print("pochodzisz z regionu EUR ale masz 18 lub mniej lat wiecj nie dostajesz super ceny dla USA ")
+elif region =="USA" and wiek <21:
     print("pochodzisz z regionu USA ale masz 21 lub mniej lat wiecj nie dostajesz super ceny dla usa")
 
 
